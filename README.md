8dd62db74a8d07e05280ef31dc54d8eab444c787
Luqaska <lucas@atico.ga>
2022-11-20 22:21:26 -0300

    v2.0.0

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 A simple private URL shortener powered by [Nim](https://nim-lang.org/).
 
+## Demo
+Want to see it working? Take a look at our [demo](https://envshort.atico.ga/).
+- [/example](https://envshort.atico.ga/example)
+- [/test](https://envshort.atico.ga/test)
+
 ## Install
 ### Getting ready the environment.
 1. Install [Nim](https://nim-lang.org/install.html) on your computer.
@@ -9,6 +14,15 @@ A simple private URL shortener powered by [Nim](https://nim-lang.org/).
 ### Compile
 3. Execute `nim c main.nim` on your PC.
 ### Configure
-4. Create environment variables like the ones listed on `.env`.
+4. Select if you want to use a JSON file or your environment variables as your database, editing the variable "`type`" at `config.json`.
+   - Option 0 => JSON file
+     - Edit `config.json`.
+     - Add keys to `urls`. The name will be the path and the content will be the address it will redirect to.
+   - Option 1 => Environment variables
+     - Take as a base `.env`.
+     - Create URLs creating variables starting with "`url_`", followed by the desired path. The value will be the address it will redirect to.
 ### Execute
-5. Execute `main.exe` if you are on Windows or run `main` if you are running Linux or Mac. It should be running at the 8080 port.
+5. Execute `main.exe` if you are on Windows or run `main` if you are running Linux or Mac.
+
+## Support
+Need help? Found a bug? Take a look at our [issues page](https://github.com/exitcas/envshort/issues)!
--- main.nim
+++ main.nim
@@ -1,36 +1,122 @@
 import prologue
-import std/[strutils, os]
+import std/[strutils, os, json]
+
+
+# Layout
+const layout = """<!DOCTYPE html>
+<html dir="ltr" lang="en">
+<head>
+<meta name="viewport" content="width=device-width" />
+<title>{{ name }}</title>
+</head>
+<body>
+<h1>{{ name }}</h1>
+<p>{{ msg }}</p>
+<p>Powered by <a href="https://github.com/exitcas/envshort">Envshort</a> v2.0.0</p>
+</body>
+</html>"""
+
+
+# Check types
+proc ctype(): int =
+  var config = parseJson(readFile("config.json"))
+  return config["type"].getInt()
+
+# Serve JSON
+proc cjson(): JsonNode =
+  var cjson = parseJson(readFile("config.json"))
+  return cjson
 
 
 # Index
 proc index(ctx: Context) {.async.} =
-  var site = readFile("template.html")
-  site = site.replace("{{ name }}", getEnv("name"))
-  site = site.replace("{{ msg }}", getEnv("description"))
+  # Set up variables
+  var site = layout
+  var name = ""
+  var description = ""
+
+  # If "type" = 1 (Use enviroment variables as database)
+  if ctype() == 1:
+    name = getEnv("name")
+    description = getEnv("description")
+  # If "type" = 0 or other value (Use JSON as database)
+  else:
+    name = cjson()["name"].getStr()
+    description = cjson()["description"].getStr()
+
+  site = site.replace("{{ name }}", name)
+  site = site.replace("{{ msg }}", description)
   resp site
+
+
 # Path managment
 proc urls(ctx: Context) {.async.} =
-  if existsEnv("url_" & ctx.getPathParams("url")):
-    resp redirect(getEnv("url_" & ctx.getPathParams("url")))
+  # Set up variables
+  var name = ""
+  var link = ""
+  var exists = false
+
+  if ctype() == 1:
+    # Check if url exists
+    if existsEnv("url_" & ctx.getPathParams("url")):
+      exists = true
+      link = getEnv("url_" & ctx.getPathParams("url"))
+    else:
+      name = getEnv("name")
   else:
-    # 404 error
-    var site = readFile("template.html")
-    site = site.replace("{{ name }}", getEnv("name"))
+    # Check if url exists
+    if cjson()["urls"].hasKey(ctx.getPathParams("url")):
+      exists = true
+      link = cjson()["urls"][ctx.getPathParams("url")].getStr()
+    else:
+      name = cjson()["name"].getStr()
+
+  if exists:
+    # Redirect if it exists
+    resp redirect(link)
+  else:
+    # Throw a 404 error if not
+    var site = layout
+    site = site.replace("{{ name }}", name)
     site = site.replace("{{ msg }}", "<b>404</b> - Not found")
     resp site, Http404
+
+
 # 500 error
 proc go500(ctx: Context) {.async.} =
-  var site = readFile("template.html")
-  site = site.replace("{{ name }}", getEnv("name"))
+  # Set up variables
+  var name = ""
+
+  if ctype() == 1:
+    name = getEnv("name")
+  else:
+    name = cjson()["name"].getStr()
+
+  var site = layout
+  site = site.replace("{{ name }}", name)
   site = site.replace("{{ msg }}", "<b>500</b> - Internal Server Error")
   resp site, Http500
 
 
+# Server settings and setup
+var port  = 8080
+var debug = false
+
+if ctype() == 1:
+  port  = parseInt(getEnv("port"))
+  debug = parseBool(getEnv("debug"))
+else:
+  port  = cjson()["port"].getInt()
+  debug = cjson()["debug"].getBool()
+
 var app = newApp(settings = newSettings(
-  port = Port(8080)
+  appName = "Envshort",
+  port    = Port(port),
+  debug   = debug
 ))
 
 app.registerErrorHandler(Http500, go500)
 app.addRoute("/", index)
 app.addRoute("/{url}$", urls)
+if not debug: echo "Envshort is serving at http://0.0.0.0:" & intToStr(port)
 app.run()
